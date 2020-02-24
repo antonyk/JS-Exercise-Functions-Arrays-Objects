c2a6fdf346dbbfbc3a846345fafa204f0014aa0f
Anthony K <antonyk@users.noreply.github.com>
2020-02-24 16:43:48 -0500

    implemented getName

--- index.js
+++ index.js
@@ -72,8 +72,10 @@ const makePersonObject = (id, name, email) => {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(obj) {
+
+  return "Hello, my name is " + obj.name;
+  
 }
 
 /**
