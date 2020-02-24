63e23b2af9a4d19c2110896d6ee63b0363d0afce
Anthony K <antonyk@users.noreply.github.com>
2020-02-24 16:44:55 -0500

    implemented getName using arrow function

--- index.js
+++ index.js
@@ -72,10 +72,14 @@ const makePersonObject = (id, name, email) => {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(obj) {
+// function getName(obj) {
 
+//   return "Hello, my name is " + obj.name;
+
+// }
+
+const getName = (obj) => {
   return "Hello, my name is " + obj.name;
-  
 }
 
 /**
