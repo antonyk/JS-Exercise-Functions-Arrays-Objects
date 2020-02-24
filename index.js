b1a2a86b7ce0650c5343ac2c8c5728e679b6bdb2
Anthony K <antonyk@users.noreply.github.com>
2020-02-24 17:40:23 -0500

    implemented getCarInfoByIndex using arrow function

--- index.js
+++ index.js
@@ -174,9 +174,14 @@ function get3rdCar(inventory) {
  * For example, if getCarInfoByIndex is invoked with the inventory and the number 0,
  * it will return `This is a Lincoln Navigator`.
 */
-function getCarInfoByIndex(inventory, index) {
+// function getCarInfoByIndex(inventory, index) {
+//   return `This is a ${inventory[index].car_make} ${inventory[index].car_model}`;
+// }
+
+let getCarInfoByIndex = (inventory, index) => {
   return `This is a ${inventory[index].car_make} ${inventory[index].car_model}`;
 }
+
 
 /**
  * ### Challenge `getLastCarInfo`
